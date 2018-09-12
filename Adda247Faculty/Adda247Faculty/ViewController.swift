6f00ed4337f26a33a9cb76cc59fe1d67dc5e08d2
Varun Tomar <varun.tomar@adda247.com>
2018-09-12 15:08:15 +0530

    iOS Faculty app project creation

---
+++ Adda247Faculty/Adda247Faculty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Adda247Faculty
+//
+//  Created by Varun Tomar on 12/09/18.
+//  Copyright © 2018 Adda247. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
